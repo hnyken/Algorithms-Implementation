baee7e63270d1cd393338f4050ca82972cbed4e5
hnyken <hi@honeymalviya.com>
2014-07-14 16:10:05 +0530

    Create Merge Sort.java

---
+++ Sorting-Algorithm/Merge Sort.java
@@ -0,0 +1,35 @@
+public int[] MergeSort(int A[], int p, int r) {
+	if (p < r) {
+		int q = (p + r) / 2;
+		A = MergeSort(A, p, q);
+		A = MergeSort(A, q + 1, r);
+		A = Merge(A, p, q, r);
+	}
+	return A;
+}
+
+private int[] Merge(int[] A, int p, int q, int r) {
+	int n1 = q - p + 1;
+	int n2 = r - q;
+	ArrayList<Integer> L = new ArrayList<Integer>();
+	for (int i = 0; i < n1; i++)
+		L.add(A[p + i]);
+	ArrayList<Integer> R = new ArrayList<Integer>();
+	for (int i = 0; i < n2; i++)
+		R.add(A[q + 1 + i]);
+	L.add(99999);
+	R.add(99999);
+	int i = 0;
+	int j = 0;
+	for (int k = p; k <= r; k++) {
+		if(L.get(i) <=R.get(j)){
+			A[k] = L.get(i);
+			i++;
+		}
+		else{
+			A[k] = R.get(j);
+			j++;
+		}
+	}
+	return A;
+}
